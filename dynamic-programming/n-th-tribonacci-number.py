d2df73e1b826c9a1041c4067d416339e609227cc
katrinars <135682193+katrinars@users.noreply.github.com>
2024-09-03 22:12:09 -0500

    LC-n-th-tribonacci-number [Runtime: N/A; Memory: N/A]

--- dynamic-programming/n-th-tribonacci-number.py
+++ dynamic-programming/n-th-tribonacci-number.py
@@ -1,19 +1,12 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        if n == 0:
-            return 0
-        if 0 < n < 3:
-            return 1
-        
-        dp = [0] * (n+1)
-
-        dp[0] = 0
-        dp[1] = 1
-        dp[2] = 1
+        T = [0] * (n+1)
+        T[0] = 0
+        T[1] = 1
+        T[2] = 1
 
         for i in range(3, n+1):
-            print(n, i, dp)
-            dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+            T[i] = T[i-3] + T[i-2] + T[i-1]
 
-        return dp[n]
+        return T[n]
         
