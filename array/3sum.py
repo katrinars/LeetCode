fe2adddea05531f988a52ace566a1dea87f39bbc
katrinars <135682193+katrinars@users.noreply.github.com>
2024-10-02 12:11:23 -0500

    LC-3sum [Runtime: N/A; Memory: N/A]

--- array/3sum.py
+++ array/3sum.py
@@ -46,7 +46,7 @@ class Solution:
                 else:
                     result.append([nums[i], nums[j], nums[k]]) # if equal, add triplet to result and move mid
                     j += 1
-                    while j < k and nums[j] == nums[j-1]: # Duplicates inside the binary search
+                    if j < k and nums[j] == nums[j-1]: # Duplicates inside the binary search
                         j += 1
         
         return result
