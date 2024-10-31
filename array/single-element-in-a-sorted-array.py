56c930f6f5ad57443f6b877ec66656261c30e984
katrinars <135682193+katrinars@users.noreply.github.com>
2024-10-30 21:57:06 -0500

    LC-single-element-in-a-sorted-array [Runtime: N/A; Memory: N/A]

--- array/single-element-in-a-sorted-array.py
+++ array/single-element-in-a-sorted-array.py
@@ -2,12 +2,14 @@ class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
         if len(nums) == 1:
             return nums[0]
-            
+
         left = 0
         right = len(nums) - 1
 
         while left <= right:
             mid = (left + right) // 2
+            if left == right or (nums[mid] != nums[mid-1] and nums[mid] != nums[mid+1]):
+                return nums[mid]
             if nums[mid] == nums[mid-1]: # mid = mid-left
                 if (mid - left - 1) % 2 == 0: # left-1 is even, search right
                     left = mid + 1
@@ -18,5 +20,5 @@ class Solution:
                     right = mid - 1
                 else: 
                     left = mid + 1
-            else:
-                return nums[mid]
+            # else:
+            #     return nums[mid]
