cf01ea755e38f855825a2f24bbe6963df90c192a
katrinars <135682193+katrinars@users.noreply.github.com>
2024-11-05 17:44:21 -0600

    LC-reverse-vowels-of-a-string [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/reverse-vowels-of-a-string.py
@@ -0,0 +1,20 @@
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        l = 0
+        r = len(s) - 1
+        vowels = ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')
+        s2 = list(s)
+
+        while l < r:
+            if s2[l] not in vowels:
+                l += 1
+            if s2[r] not in vowels:
+                r -= 1
+            else:
+                s2[l], s2[r] = s2[r], s2[l]
+                l += 1
+                r -= 1
+        
+        return ''.join(s2)
+
+
