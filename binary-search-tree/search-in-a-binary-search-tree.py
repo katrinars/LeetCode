e597ad847b991ef1c6e8921bb25027db30608325
katrinars <135682193+katrinars@users.noreply.github.com>
2024-11-05 21:44:14 -0600

    LC-search-in-a-binary-search-tree [Runtime: 0 ms; Memory: 18.6 MB]

---
+++ binary-search-tree/search-in-a-binary-search-tree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        current = root
+        while current:
+            if val < current.val:
+                current = current.left
+            elif val > current.val:
+                current = current.right
+            else:
+                return current
+
+        return 
