4d6304b6de269a9a622a0de06d3385c7992337bd
katrinars <135682193+katrinars@users.noreply.github.com>
2025-03-29 17:37:52 -0500

    LC-happy-number [Runtime: N/A; Memory: N/A]

---
+++ hash-table/happy-number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        # print(n)
+        if n  == 1: return True
+        elif n <= 3: return False
+        else:
+            return self.isHappy((n % 10)**2 + self.getDigits(n // 10))
+            
+   
+    def getDigits(self, n):
+        if n < 10:
+            return n**2
+        else:
+            return (n % 10)**2 + self.getDigits(n // 10)
