edcba3128209c6eb0af9f552ad2fa8a868b33805
katrinars <135682193+katrinars@users.noreply.github.com>
2025-04-05 09:46:46 -0500

    LC-merge-sorted-array [Runtime: N/A; Memory: N/A]

--- array/merge-sorted-array.py
+++ array/merge-sorted-array.py
@@ -3,9 +3,20 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        for num in nums2:
-            nums1.insert(0, num)
-        while len(nums1) > m+n:
-            nums1.pop(-1)
-        nums1.sort()
+        if not nums2: return
+        p1 = p2 = 0
+        while p1 < len(nums1) and p2 < len(nums2):
+            if nums1[p1] == 0:
+                break
+            if nums1[p1] > nums2[p2]:
+                nums1.insert(p1, nums2[p2])
+                p1 += 1
+                p2 += 1
+            else:
+                # nums1.insert(p1+1, nums2[p2])
+                p1 += 1
+                # p2 += 1
+        if p2 < len(nums2):
+            nums1[p1:] = nums2[p2:]
+
             
