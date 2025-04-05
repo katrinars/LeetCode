53fcb822d04e018a00198b15a3333783040d4db9
katrinars <135682193+katrinars@users.noreply.github.com>
2025-04-05 11:04:01 -0500

    LC-word-pattern [Runtime: N/A; Memory: N/A]

---
+++ hash-table/word-pattern.py
@@ -0,0 +1,17 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        match = {}
+        i = 0
+        s = s.split()
+
+        while i < len(pattern):
+            if pattern[i] in match:
+                if match[pattern[i]] != s[i]:
+                    return False
+            else:
+                if s[i] not in match.values():
+                    match[pattern[i]] = s[i]
+                else:
+                    return False
+            i += 1
+        return True
