fdee455b884c00eeccfe6f5d4ba9201b2ea740a1
katrinars <135682193+katrinars@users.noreply.github.com>
2025-04-05 09:57:26 -0500

    LC-remove-element [Runtime: N/A; Memory: N/A]

--- array/remove-element.py
+++ array/remove-element.py
@@ -1,12 +1,16 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        # while val in nums:
-        #     nums.remove(val)
-        # return len(nums)
+        l = 0
+        r = len(nums)-1
 
-        while nums:
-            try:
-                nums.remove(val)
-            except ValueError:
-                return len(nums)
+        while l < r:
+            if nums[r] == val:
+                r -= 1
+            elif nums[l] == val:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+                r -= 1
+            else: 
+                l += 1
+        return l+1
             
