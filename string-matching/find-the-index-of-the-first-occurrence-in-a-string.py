c9fc9d5bb62603cf366a86277b2b19f6ca9f4359
katrinars <135682193+katrinars@users.noreply.github.com>
2025-04-17 19:19:13 -0500

    LC-find-the-index-of-the-first-occurrence-in-a-string [Runtime: 0 ms; Memory: 17.8 MB]

--- string-matching/find-the-index-of-the-first-occurrence-in-a-string.py
+++ string-matching/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,3 +1,11 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        return haystack.find(needle)
+        l = 0
+        r = len(needle) - 1
+
+        while r < len(haystack):
+            if haystack[l:r+1] == needle:
+                return l
+            l += 1
+            r += 1
+        return -1
