6f275b1c7f838690b5f03bcbb4ecf254bb229838
katrinars <135682193+katrinars@users.noreply.github.com>
2026-03-02 12:38:07 -0600

    LC-maximum-average-subarray-i [Runtime: N/A; Memory: N/A]

--- array/maximum-average-subarray-i.py
+++ array/maximum-average-subarray-i.py
@@ -2,16 +2,17 @@ class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
         s = 0
         e = k 
-        max_avg = float("-inf")
+        highest_sum = float("-inf")
+        # max_avg = float("-inf")
 
         if len(nums) == k:
             return sum(nums) / k 
 
         while e < len(nums) + 1: 
-            average = sum(nums[s:e]) / k 
-            max_avg = max(max_avg, average) 
+            new_sum = sum(nums[s:e])
+            highest_sum = max(max_avg, average) 
 
             s += 1
             e += 1
 
-        return max_avg
+        return highest_sum / k
